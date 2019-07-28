07cf50878e196c1623f622b7dfa3fc13465171fb
deva5610 <40216384+deva5610@users.noreply.github.com>
2019-07-28 19:15:52 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,46 @@
+# IMDBList2PlexCollection
+Simple script to take an IMDB list, match the movies in your Plex Library and turn them into a collection.
+
+This script is a modified version of [this excellent script](https://gist.github.com/JonnyWong16/f5b9af386ea58e19bf18c09f2681df23).
+
+Thanks to /u/SwiftPanda16 for the original.
+
+# Disclaimer
+I'm not a developer.....at all. My modifications are probably quite slap happy, but they work fine for me and have on a few
+different installs now with 0 problems. **This doesn't mean it will for you.** I'm not responsible for any heartaches caused when you
+decide to mess with your Plex server. Maybe spin up a small test library before deploying it on a big library if you're concerned
+about my lack of ability!
+
+# Installation
+Edit imdb2collection.py with your favourite text editor. PLEX_URL, PLEX_TOKEN and MOVIE_LIBRARIES need to be set.
+
+PLEX_URL cannot end with a trailing slash - http://localhost:32400 & https://plex.woofwoof.wahoo are both fine,
+https://plex.woofwoof.wahoo/ is not.
+
+PLEX_TOKEN can be found using [this guide.](https://support.plex.tv/articles/204059436-finding-an-authentication-token-x-plex-token/)
+A token can also be found in Tautulli or Ombi if you're using them.
+
+MOVIE_LIBRARIES is pretty self explanatory. ['My Movies 1'], ['My Movies 1', 'My Movies 2'] are both valid examples.
+
+They are the three variables most people will have to fill in. If you're using 'The Movie Database' agent instead of Plex Movie you'll
+also need to edit the TMDB_API_KEY variable.
+
+That's all. Nothing else needs to be edited.
+
+# Usage
+Use pip to install the few listed requirements.
+
+pip install -r requirements.txt
+
+Run the script with "python imdb2collection.py" and follow the instructions. You'll want two things. A URL to the IMDB list you want to
+use to match (eg - https://www.imdb.com/list/ls064646512/) and to decide what you want the matching movies to be tagged as
+(eg - Pixar, Pixar Movies, Pixar Animations, etc - all 3 are valid entries when asked).
+
+That's it. The script should (hopefully!) run, it'll match movies from the IMDB list to your Movies Library and tag them into the
+collection you specified.
+
+# Issues
+Probably. Don't blame me. Fork, fix and merge.
+
+# Enjoy
+This one is simple.
---
+++ imdb2collection.py
@@ -0,0 +1,190 @@
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+import json
+import requests
+import time
+from lxml import html
+from plexapi.server import PlexServer
+
+#------------------------------------------------------------------------------
+#
+#	  Automated IMDB List to Plex Collection Script by /u/deva5610
+#
+#		       Created by modifiying the excellent
+#
+#       Automated IMDB Top 250 Plex collection script by /u/SwiftPanda16
+#
+#                         *** USE AT YOUR OWN RISK! ***
+#   *** I AM NOT RESPONSIBLE FOR DAMAGE TO YOUR PLEX SERVER OR LIBRARIES! ***
+#
+#------------------------------------------------------------------------------
+
+#############################
+### EDIT THE BELOW VALUES ###
+#############################
+
+### Plex server details ###
+
+PLEX_URL = 'https://plex.blah' 		#Plex URL without trailing slash.
+PLEX_TOKEN = ''
+MOVIE_LIBRARIES = ['Test']			#Enter Plex movie libraries, multiple allowed like ['Movies2', 'Test Movies'].
+
+
+### TMBD API Key if using "The Movie Database" agent ###
+
+TMDB_API_KEY = ''
+
+#############################################
+##### CODE BELOW - DON'T EDIT BELOW HERE#####
+#############################################
+
+### Header ###
+
+print("===================================================================")
+print(" Automated IMDB List to Collection script by /u/deva5610  ")
+print(" Created by modifiying the excellent  ")
+print(" Automated IMDB Top 250 Plex collection script by /u/SwiftPanda16  ")
+print("===================================================================")
+print("\n")
+
+###IMDB List Details###
+
+IMDB_URL = input("IMDB List URL (eg - https://www.imdb.com/list/ls002400902/): ")
+IMDB_COLLECTION_NAME = input("Collection Name (eg - Disney Classics): ")
+
+TMDB_REQUEST_COUNT = 0  # DO NOT CHANGE
+
+def add_collection(library_key, rating_key):
+    headers = {"X-Plex-Token": PLEX_TOKEN}
+    params = {"type": 1,
+              "id": rating_key,
+              "collection[0].tag.tag": IMDB_COLLECTION_NAME,
+              "collection.locked": 1
+              }
+
+    url = "{base_url}/library/sections/{library}/all".format(base_url=PLEX_URL, library=library_key)
+    r = requests.put(url, headers=headers, params=params)
+
+
+def remove_collection(library_key, rating_key):
+    headers = {"X-Plex-Token": PLEX_TOKEN}
+    params = {"type": 1,
+              "id": rating_key,
+              "collection[].tag.tag-": IMDB_COLLECTION_NAME
+              }
+
+    url = "{base_url}/library/sections/{library}/all".format(base_url=PLEX_URL, library=library_key)
+    r = requests.put(url, headers=headers, params=params)
+
+
+def get_imdb_id_from_tmdb(tmdb_id):
+    global TMDB_REQUEST_COUNT
+    
+    if not TMDB_API_KEY:
+        return None
+    
+    # Wait 10 seconds for the TMDb rate limit
+    if TMDB_REQUEST_COUNT >= 40:
+        time.sleep(10)
+        TMDB_REQUEST_COUNT = 0
+    
+    params = {"api_key": TMDB_API_KEY}
+    
+    url = "https://api.themoviedb.org/3/movie/{tmdb_id}".format(tmdb_id=tmdb_id)
+    r = requests.get(url, params=params)
+    
+    TMDB_REQUEST_COUNT += 1
+    
+    if r.status_code == 200:
+        movie = json.loads(r.text)
+        return movie['imdb_id']
+    else:
+        return None
+    
+    
+def run_imdb_sync():
+    try:
+        plex = PlexServer(PLEX_URL, PLEX_TOKEN)
+    except:
+        print("No Plex server found at: {base_url}".format(base_url=PLEX_URL))
+        print("Exiting script.")
+        return [], 0
+
+    # Get list of movies from the Plex server
+    all_movies = []
+    for movie_lib in MOVIE_LIBRARIES:
+        try:
+            print("Retrieving a list of movies from the '{library}' library in Plex...".format(library=movie_lib))
+            movie_library = plex.library.section(movie_lib)
+            library_language = movie_library.language  # IMDB will use language from last library in list
+            all_movies.extend(movie_library.all())
+        except:
+            print("The '{library}' library does not exist in Plex.".format(library=movie_lib))
+            print("Exiting script.")
+            return [], 0
+
+    # Get the requested imdb list
+    print("Retrieving movies from selected IMDB list.")
+    r = requests.get(IMDB_URL, headers={'Accept-Language': library_language})
+    tree = html.fromstring(r.content)
+    title_name = tree.xpath("//div[contains(@class, 'lister-item-content')]//h3[contains(@class, 'lister-item-header')]//a/text()")
+    title_years = tree.xpath("//div[contains(@class, 'lister-item-content')]//h3[contains(@class, 'lister-item-header')]//span[contains(@class, 'lister-item-year')]/text()")
+    title_ids = tree.xpath("//div[contains(@class, 'lister-item-content')]//div[contains(@class, 'ipl-rating-interactive')]/input//@data-tconst")
+
+    # Create a dictionary of {imdb_id: movie}
+    imdb_map = {}
+    for m in all_movies:
+        if 'imdb://' in m.guid:
+            imdb_id = m.guid.split('imdb://')[1].split('?')[0]
+        elif 'themoviedb://' in m.guid:
+            tmdb_id = m.guid.split('themoviedb://')[1].split('?')[0]
+            imdb_id = get_imdb_id_from_tmdb(tmdb_id)
+        else:
+            imdb_id = None
+            
+        if imdb_id and imdb_id in title_ids:
+            imdb_map[imdb_id] = m
+        else:
+            imdb_map[m.ratingKey] = m
+
+    # Add movies to the selected collection
+    print("Adding the collection '{}' to movies on the selected IMDB list...".format(IMDB_COLLECTION_NAME))
+    in_library_idx = []
+    for i, imdb_id in enumerate(title_ids):
+        movie = imdb_map.pop(imdb_id, None)
+        if movie:
+            add_collection(movie.librarySectionID, movie.ratingKey)
+            in_library_idx.append(i)
+
+### See no reason to remove movies yet. Commenting this section out for now. ###
+#            
+#    # Remove movies from collection with are no longer on the IMDB Top 250 list
+#    print("Removing the collection '{}' from movies not on the IMDB Top 250 list...".format(IMDB_COLLECTION_NAME))
+#    count = 0
+#    for movie in imdb_map.values():
+#        remove_collection(movie.librarySectionID, movie.ratingKey)
+    
+    # Get list of missing movies from selected list
+    missing_imdb_movies = [(idx, imdb) for idx, imdb in enumerate(zip(title_ids, title_name, title_years))
+                        if idx not in in_library_idx]
+
+    return missing_imdb_movies, len(title_ids)
+
+
+if __name__ == "__main__":
+
+    missing_imdb_movies, list_count = run_imdb_sync()
+    
+    print("\n===================================================================\n")
+    print("Number of IMDB movies from selected list in the library: {count}".format(count=list_count-len(missing_imdb_movies)))
+    print("Number of missing selected list movies: {count}".format(count=len(missing_imdb_movies)))
+    print("\nList of movies missing that are in selected IMDB list:\n")
+    
+    for idx, (imdb_id, title, year) in missing_imdb_movies:
+        print("{idx}\t{imdb_id}\t{title} {year}".format(idx=idx+1, imdb_id=imdb_id, title=title.encode('UTF-8'), year=year))
+    
+    print("\n===================================================================")
+    print("                               Done!                               ")
+    print("===================================================================\n")
+    
+    input("Press Enter to finish...")
