79f7d91caf4aec8069526ea815cdf67e39b9c441
deva5610 <40216384+deva5610@users.noreply.github.com>
2019-07-28 21:03:58 +0800

    Update imdb2collection.py
    Testing using IMDB compact view

--- imdb2collection.py
+++ imdb2collection.py
@@ -129,9 +129,9 @@ def run_imdb_sync():
     print("Retrieving movies from selected IMDB list.")
     r = requests.get(IMDB_URL, headers={'Accept-Language': library_language})
     tree = html.fromstring(r.content)
-    title_name = tree.xpath("//div[contains(@class, 'lister-item-content')]//h3[contains(@class, 'lister-item-header')]//a/text()")
-    title_years = tree.xpath("//div[contains(@class, 'lister-item-content')]//h3[contains(@class, 'lister-item-header')]//span[contains(@class, 'lister-item-year')]/text()")
-    title_ids = tree.xpath("//div[contains(@class, 'lister-item-content')]//div[contains(@class, 'ipl-rating-interactive')]/input//@data-tconst")
+    title_name = tree.xpath("//div[contains(@class, 'lister-col-wrapper')]//span[contains(@class, 'lister-item-header')]//a/text()")
+    title_years = tree.xpath("//div[contains(@class, 'lister-col-wrapper')]//span[contains(@class, 'lister-item-header')]//span[contains(@class, 'lister-item-year')]/text()")
+    title_ids = tree.xpath("//div[contains(@class, 'lister-col-wrapper')]//div[contains(@class, 'ipl-rating-interactive')]/input//@data-tconst")
 
     # Create a dictionary of {imdb_id: movie}
     imdb_map = {}
